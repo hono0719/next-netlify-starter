a284e984690ac540df5ca6730af0d770b25bbe3a
hono0719 <105632494+hono0719@users.noreply.github.com>
2022-06-07 22:01:46 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,28 +4,20 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-//     <div className="container">
-//       <Head>
-//         <title>Next.js Starter!</title>
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
 
-    
-      <!DOCTYPE html>
-      <html>
-        <head>
-        <meta charset="UTF-8">
-        <script src="loc1.js"></script>
-        </head>
-        <body>
-        <H2>Testing Geolocation API: getCurrentPosition()</H2>
-        <dl id="result"></dl>
-        </body>
-      </html>
-
-
-//       <Footer />
-//     </div>
+      <Footer />
+    </div>
   )
 }
